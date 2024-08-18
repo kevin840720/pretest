69c33be73a2ba7a01edfe76e13c9acd330e52a3c
Kevin.Wang <kevin840720@gmail.com>
2024-08-18 13:57:08 +0800

    [test]: add price test for order  api

--- test/test_app.py
+++ test/test_app.py
@@ -79,6 +79,46 @@ def test_make_order_violate_name_non_english(client):
     assert response.status_code == 400
     assert response.data.decode('utf-8') == "Name contains non-English characters"
 
+def test_make_order_price_over_2000_boundary(client):
+    obj = {"id":"A0000001",
+           "name":"Melody Holiday Inn",
+           "address":{"city":"taipei-city",
+                      "district":"da-an-district",
+                      "street":"fuxing-south-road",
+                      },
+           "price":"2000",
+           "currency":"TWD",
+           }
+    response = client.post("/api/orders",
+                           data=json.dumps(obj),
+                           content_type="application/json",
+                           )
+
+    text = deepcopy(obj)
+    text["price"] = 2000
+
+    assert response.status_code == 200
+    assert json.loads(response.data.decode('utf-8')) == text
+
+
+def test_make_order_price_over_2000(client):
+    obj = {"id":"A0000001",
+           "name":"Melody Holiday Inn",
+           "address":{"city":"taipei-city",
+                      "district":"da-an-district",
+                      "street":"fuxing-south-road",
+                      },
+           "price":"2001",
+           "currency":"TWD",
+           }
+    response = client.post("/api/orders",
+                           data=json.dumps(obj),
+                           content_type="application/json",
+                           )
+
+    assert response.status_code == 400
+    assert response.data.decode('utf-8') == "Price is over 2000"
+
 def test_make_order_violate_name_not_capital(client):
     obj = {"id":"A0000001",
            "name":"Melody holiday Inn",
